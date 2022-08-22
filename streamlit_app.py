5aaa6ba15875ad1952e09e8bf0fed105ce2d35a5
PrajaktaPawar <111564558+PrajPawar13@users.noreply.github.com>
2022-08-22 16:08:38 +0530

    Update streamlit_app.py
    added 3 menu in breakfast list

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title('My parents new Menu')
 streamlit.header(' Breakfast Menu')
-streamlit.text(' Bleuberry Oatmeal')
-streamlit.text(' Spinach Smoothie')
-streamlit.text(' Boiled Egg')
+streamlit.text(' 🥣Bleuberry Oatmeal')
+streamlit.text(' 🥗Spinach Smoothie')
+streamlit.text(' 🐔Boiled Egg')
+streamlit.text(' 🥑Avacado')
