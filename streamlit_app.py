3ec3fb7efac353551c7fd75d4f7c2cbea25afbdf
PrajaktaPawar <111564558+PrajPawar13@users.noreply.github.com>
2022-08-22 16:21:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
 
-streamlit.title('My parents new Menu')
-streamlit.header(' Breakfast Menu')
+streamlit.title('My Mom's new Healthy Dinner')
+streamlit.header(' Breakfast Favorites')
 streamlit.text(' 🥣Bleuberry Oatmeal')
 streamlit.text(' 🥗Spinach Smoothie')
 streamlit.text(' 🐔Boiled Egg')
 streamlit.text(' 🥑Avacado')
+
+                
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
